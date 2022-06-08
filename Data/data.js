0a97578fc6a031483b1a459928e03cd739fd526e
tranvanhieu2002 <hieu.tran23@student.passerellesnumeriques.org>
2022-06-08 19:05:10 +0700

    update data

--- Data/data.js
+++ Data/data.js
@@ -1,6 +1,23 @@
 
 module.exports = () => ({
-  nhaXe1: require('./data1.json'),
-  nhaXe2: require('./data2.json')
-  // Something more
+   data: [
+      {
+         id:1,
+         name:"Trang web Vé xe  rẻ",
+         web: require('./data1.json')
+      },
+      {
+         id:2,
+         name: "Trang web hôm nay tới lượt anh",
+         web: require('./data2.json')
+         
+      },
+      {  
+         id:3,
+         name: "Trang web hôm nay là thứ 7",
+         web: require('./data3.json')
+         
+      }
+      // Something more
+    ]
 });
--- index.js
+++ index.js
@@ -7,4 +7,5 @@ server.use(middlewares)
 server.use(router)
 server.listen(4000, () => {
   console.log('JSON Server is running')
+  console.log('It change ne');
 })
