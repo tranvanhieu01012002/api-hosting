cf447c8212eaa0575fd9ddf76a246efa73c0e31a
tranvanhieu2002 <hieu.tran23@student.passerellesnumeriques.org>
2022-06-17 14:33:22 +0700

    test code

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router(require("./Data/data")())
 const middlewares = jsonServer.defaults()
-console.log("ffffffff");
 server.use(middlewares)
 server.use(router)
+
+var today = new Date();
+var date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var dateTime = date+' '+time;
+
 server.listen(4000, () => {
   console.log('JSON Server is running')
-  console.log('It change ne-');
+  console.log(dateTime);
 })
